15d2468e53ad9d6e77273c8e7de09b6122f0715e
Ubuntu <ubuntu@ip-172-31-27-10.eu-central-1.compute.internal>
2019-11-15 13:52:17 +0000

    Added the right command to execute. The required executions are commented out.

--- run_part_1.py
+++ run_part_1.py
@@ -3,8 +3,9 @@
 from organized_all import run_part_1
 
 # TODO decide what command line params we need
-#[arg1, arg2] = sys.argv[1:]
+[merged_input_fname, sine_name, output_dir] = sys.argv[1:]
 
+run_all(merged_input_fname, sine_name, output_dirq)
 
 # run_all("/media/sf_gene/unified/wt-lung_unified_001.fastq.gz",
 #         "/media/sf_gene/original/B1.fasta",
@@ -33,49 +34,49 @@ from organized_all import run_part_1
 
 
 
-run_part_1(f"/media/sf_gene/original/wt-lung_R1_001.fastq.gz",
-        "/media/sf_gene/original/B1.fasta",
-        "/media/sf_gene/B1-wt_R1")
+#run_part_1(f"/media/sf_gene/original/wt-lung_R1_001.fastq.gz",
+#        "/media/sf_gene/original/B1.fasta",
+#        "/media/sf_gene/B1-wt_R1")
 
-run_part_1("/media/sf_gene/original/wt-lung_R2_001.fastq.gz",
-        "/media/sf_gene/original/B1.fasta",
-        "/media/sf_gene/B1-wt_R2")
+#run_part_1("/media/sf_gene/original/wt-lung_R2_001.fastq.gz",
+#        "/media/sf_gene/original/B1.fasta",
+#        "/media/sf_gene/B1-wt_R2")
 
-run_part_1("/media/sf_gene/original/wt-lung_R1_001.fastq.gz",
-        "/media/sf_gene/original/B2.fasta",
-        "/media/sf_gene/B2-wt_R1")
+#run_part_1("/media/sf_gene/original/wt-lung_R1_001.fastq.gz",
+#        "/media/sf_gene/original/B2.fasta",
+#        "/media/sf_gene/B2-wt_R1")
 
-run_part_1("/media/sf_gene/original/wt-lung_R2_001.fastq.gz",
-        "/media/sf_gene/original/B2.fasta",
-        "/media/sf_gene/B2-wt_R2")
+#run_part_1("/media/sf_gene/original/wt-lung_R2_001.fastq.gz",
+#        "/media/sf_gene/original/B2.fasta",
+#        "/media/sf_gene/B2-wt_R2")
 
-run_part_1("/media/sf_gene/original/wt-lung_R1_001.fastq.gz",
-        "/media/sf_gene/original/B4.fasta",
-        "/media/sf_gene/B4-wt_R1")
+#run_part_1("/media/sf_gene/original/wt-lung_R1_001.fastq.gz",
+#        "/media/sf_gene/original/B4.fasta",
+#        "/media/sf_gene/B4-wt_R1")
 
-run_part_1("/media/sf_gene/original/wt-lung_R2_001.fastq.gz",
-        "/media/sf_gene/original/B4.fasta",
-        "/media/sf_gene/B4-wt_R2")
+#run_part_1("/media/sf_gene/original/wt-lung_R2_001.fastq.gz",
+#        "/media/sf_gene/original/B4.fasta",
+#        "/media/sf_gene/B4-wt_R2")
 
-run_part_1("/media/sf_gene2/original_old/old_lung_R1_001.fastq.gz",
-        "/media/sf_gene/original/B1.fasta",
-        "/media/sf_gene2/B1-old_R1")
+#run_part_1("/media/sf_gene2/original_old/old_lung_R1_001.fastq.gz",
+#        "/media/sf_gene/original/B1.fasta",
+#        "/media/sf_gene2/B1-old_R1")
 
-run_part_1("/media/sf_gene2/original_old/old_lung_R2_001.fastq.gz",
-        "/media/sf_gene/original/B1.fasta",
-        "/media/sf_gene2/B1-old_R2")
+#run_part_1("/media/sf_gene2/original_old/old_lung_R2_001.fastq.gz",
+#        "/media/sf_gene/original/B1.fasta",
+#        "/media/sf_gene2/B1-old_R2")
 
-run_part_1("/media/sf_gene2/original_old/old_lung_R1_001.fastq.gz",
-        "/media/sf_gene/original/B2.fasta",
-        "/media/sf_gene2/B2-old_R1")
+#run_part_1("/media/sf_gene2/original_old/old_lung_R1_001.fastq.gz",
+#        "/media/sf_gene/original/B2.fasta",
+#        "/media/sf_gene2/B2-old_R1")
 
-run_part_1("/media/sf_gene2/original_old/old_lung_R2_001.fastq.gz",
-        "/media/sf_gene/original/B2.fasta",
-        "/media/sf_gene2/B2-old_R2")
+#run_part_1("/media/sf_gene2/original_old/old_lung_R2_001.fastq.gz",
+#        "/media/sf_gene/original/B2.fasta",
+#        "/media/sf_gene2/B2-old_R2")
 
-run_part_1("/media/sf_gene2/original_old/old_lung_R1_001.fastq.gz",
-        "/media/sf_gene/original/B4.fasta",
-        "/media/sf_gene2/B4-old_R1")
+#run_part_1("/media/sf_gene2/original_old/old_lung_R1_001.fastq.gz",
+#        "/media/sf_gene/original/B4.fasta",
+#        "/media/sf_gene2/B4-old_R1")
 
 run_part_1("/media/sf_gene2/original_old/old_lung_R2_001.fastq.gz",
         "/media/sf_gene/original/B4.fasta",
