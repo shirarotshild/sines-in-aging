00495489c9877aefd7d6c16397d2804ebe693207
Ubuntu <ubuntu@ip-172-31-27-10.eu-central-1.compute.internal>
2019-11-19 07:45:31 +0000

    split_recompress: write gzip instead of Zstandard

--- split_recompress.py
+++ split_recompress.py
@@ -1,6 +1,7 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.6
 import gzip
-import zstandard as zstd
+import sys
+#import zstandard as zstd
 import itertools
 import time
 import os
@@ -18,25 +19,26 @@ def split_recompress(basename, skip=[]):
     in_fname = f'{basename}.fastq.gz'
     with gzip.open(in_fname, 'rb') as in_fastq:
         for i, chunk in enumerate(chunks(in_fastq, 100_000_000)):
-            out_fname = f'{basename}.part{i}e8.fastq.zst'
+            out_fname = f'{basename}.part{i}e8.fastq.gz'
             if i in skip:
                 print('skipping', out_fname)
                 max(chunk)  # consume iterator
                 continue
             print('writing', out_fname)
             t0 = time.time()
-            with open(out_fname, 'wb') as out_fastq_zstd:
-                cctx = zstd.ZstdCompressor(level=12, threads=4)
-                with cctx.stream_writer(out_fastq_zstd) as out_fastq:
-                    # a line-by-line python loop is subotimal, but ~60% CPU is spent in
-                    # ZSTD_compressBlock_lazy2_extDict so good enough for a one-off.
-                    for line in chunk:
-                        out_fastq.write(line)
+            with gzip.open(out_fname, 'wb', compresslevel=6) as out_fastq_gz:
+            #with open(out_fname, 'wb') as out_fastq_gz:
+                # a line-by-line python loop is subotimal, but ~60% CPU is spent in
+                # ZSTD_compressBlock_lazy2_extDict so good enough for a one-off.
+                for line in chunk:
+                    out_fastq_gz.write(line)
             t1 = time.time()
             print(t1 - t0, 'sec.', 'Last line:', line)
     os.remove(in_fname)
 
-split_recompress('Old-lung/old_lung_R1_001')
-split_recompress('Old-lung/old_lung_R2_001')
-split_recompress('Young-lung/wt-lung_R1_001')
-split_recompress('Young-lung/wt-lung_R2_001')
+[in_filename_base] = sys.argv[1:]
+split_recompress(in_filename_base)
+#split_recompress('Old-lung/old_lung_R1_001')
+#split_recompress('Old-lung/old_lung_R2_001')
+#split_recompress('Young-lung/wt-lung_R1_001')
+#split_recompress('Young-lung/wt-lung_R2_001')
