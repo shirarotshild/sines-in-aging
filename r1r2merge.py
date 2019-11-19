d972e868d78db1c122036efe9b0bbac737cf408e
Ubuntu <ubuntu@ip-172-31-27-10.eu-central-1.compute.internal>
2019-11-19 08:09:22 +0000

    r1r2merge.py: write .gz

--- r1r2merge.py
+++ r1r2merge.py
@@ -2,6 +2,7 @@
 import math
 import tre
 import sys
+import gzip
 import sines_io
 import gene_lib
 from sines_io import fastq_zst_records
@@ -51,39 +52,37 @@ def merged_paired_ends(records1, records2):
             #        print('fuzzyness = ', fuzzyness)
                 tot_great += 1
                 # An arbitrary decision: take the common string from r2
-                res_str = end1[ : -to_search_len] + str2 
+                res_str = end1[ : -to_search_len] + str2
+                # TODO: preserve qualities
                 res_seq = SeqRecord(Seq(res_str), id=rec1.id, name=rec1.name, description = rec1.description, letter_annotations =
                                     {"phred_quality":[30 for i in range(len(res_str))]})
                 if (tot_great % step == 0):
                     print('nicely matched ',str1,'\n',str2, to_search_len, match_tot.group(0), match.group(0), match_tot.cost, match.cost)
                 yield res_seq
+                continue
 
-        res_str = str1 + ''.join(['N' for i in range(padding)]) + str2
+        res_str = str1 + ('N' * padding) + str2
         res_seq = SeqRecord(Seq(res_str), id=rec1.id, name=rec1.name,
                    description = rec1.description, letter_annotations = {"phred_quality":[30 for i in range(len(res_str))]}) 
         if  (tot % step == 0):
             print (tot, tot_good, tot_great)
            # print('matched ',str1,'\n',str2, len(str1), len(str2))
         yield res_seq
-                
 
-                
-            
+
 ### MAIN ###
-[base1, base2, out_base] = sys.argv[1:]
+[in_fname1, in_fname2, out_fname] = sys.argv[1:]
 def write_merged():
-    out_fname = f'{out_base}.merged.fastq'
-    in_fname1 = f'{base1}.fastq.gz'
-    in_fname2 = f'{base2}.fastq.gz'
     # We assume for now all records match. Generally, need to verify.
     print('About to merge ',in_fname1, in_fname2)
     with gene_lib.open_any(in_fname1, 'rt') as in_f1_handle:
-        records1 = SeqIO.parse(in_f1_handle,"fastq")
+        records1 = SeqIO.parse(in_f1_handle, format="fastq")
         with gene_lib.open_any(in_fname2, 'rt') as in_f2_handle:
-            records2 = SeqIO.parse(in_f2_handle,"fastq")
+            records2 = SeqIO.parse(in_f2_handle, format="fastq")
             merged = merged_paired_ends(records1, records2)
-            print('about to write ', out_fname + '.tmp')
-            Bio.SeqIO.write(merged, out_fname + '.tmp', 'fastq')
+            print('about to write ', out_fname + '[.tmp]')
+            with gzip.open(out_fname + '.tmp', 'wt', compresslevel=6) as out_handle:
+                Bio.SeqIO.write(merged, handle=out_handle, format='fastq')
     os.rename(out_fname + '.tmp', out_fname)
 
 write_merged()
