c2f1e502c4da6935f243aaa68d012e79c10ca56c
Anat Paskin-Cherniavsky <anps83@gmail.com>
2020-08-11 18:14:04 +0300

    filter_candidates: look for any of several SINEs or their RC at once
    Giving up as this proved to have very close performance to just running filter_candidates several times :-(
    
    Having candidates of different SINEs mixed in same output
    file is not a big issue, as running it again to extract matches of only one kind would be fast (~15 times smaller input), but it's still a nuisance.

--- filter_candidates.py
+++ filter_candidates.py
@@ -5,23 +5,29 @@ import tre
 
 import Bio
 from Bio import SeqIO
-#from Bio.Seq import Seq
-#from Bio.Alphabet import IUPAC
-#from Bio.SeqRecord import SeqRecord
+from Bio.Seq import Seq
 
 import gene_lib
 from gene_lib import log
 from gene_lib import get_sine_forward
 
-def filter_potential_sines(in_fname, sine_string, sine_header=67, maxerr=19):
+def filter_potential_sines(in_fname, sine_fname_list, sine_header=67, maxerr=19):
     """
     Finds candidate SINEs with a certain distance from a prefix length.
     To be used for preliminary screening (input for later steps).
     """
+    regexp_parts = []
+    for sine_fname in sine_fname_list:
+        cur_sine = gene_lib.get_sine_forward(sine_fname)[:sine_header]
+        cur_sine_rc = str(Seq(cur_sine).reverse_complement())
+        regexp_parts += [cur_sine, cur_sine_rc]
+    regexp = '|'.join(regexp_parts)
+    
+    re = tre.compile(regexp, tre.EXTENDED)
+    fuzziness = tre.Fuzzyness(maxerr=maxerr)
+    
     with gene_lib.open_any(in_fname, 'rt') as in_file_handle:
         records = SeqIO.parse(in_file_handle, format="fastq")
-        re = tre.compile(sine[:sine_header], tre.EXTENDED)
-        fuzziness = tre.Fuzzyness(maxerr=maxerr)
 
         for rec in records:
             match = re.search(str(rec.seq), fuzziness)
@@ -30,8 +36,7 @@ def filter_potential_sines(in_fname, sine_string, sine_header=67, maxerr=19):
                 #sine_location = match.groups() #returns tuple of tuples (in this case: ((2,78), ) for example
                 SeqIO.write(rec, sys.stdout, 'fasta')
 
-# Writes to stdout, uncompresed
-[sine_fname, merged_input_fname] = sys.argv[1:]
-log('About to screen', merged_input_fname, 'for', sine_fname)
-sine = gene_lib.get_sine_forward(sine_fname) #"B1.fasta"
-filter_potential_sines(merged_input_fname, sine)
+# Writes to stdout, uncompresed .fasta
+[*sine_fname_list, merged_input_fname] = sys.argv[1:]
+log(f'About to screen {merged_input_fname} for any of {sine_fname_list}, direct or reverse-complement.')
+filter_potential_sines(merged_input_fname, sine_fname_list)
